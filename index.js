e70459facf3499382ddac9f11a0e6914a53e10f8
husnu-coban <husnucoban5h@gmail.com>
2023-05-09 23:39:32 +0300

    analog clock v1

---
+++ index.js
@@ -0,0 +1,30 @@
+const akrep=document.getElementsByClassName("akrep");
+const akrep2=document.getElementsByClassName("akrep2");
+const yelkovan=document.getElementsByClassName("yelkovan");
+const yelkovan2=document.getElementsByClassName("yelkovan2");
+const saniye=document.getElementsByClassName("saniye");
+const saniye2=document.getElementsByClassName("saniye2");
+
+zaman();
+
+function zaman(){
+    const now=new Date();
+    const hour=now.getHours();
+    const minute=now.getMinutes();
+    const second=now.getSeconds();
+
+    console.log(hour);
+
+    akrep[0].style.transform=`rotate(${(hour/24)*360}deg)`;
+    akrep[1].style.transform=`rotate(${(hour/24)*360}deg)`;
+    akrep2[0].style.transform=`rotate(${(hour/24)*360}deg)`;
+
+    yelkovan[0].style.transform=`rotate(${(minute/60)*360}deg)`;
+    yelkovan[1].style.transform=`rotate(${(minute/60)*360}deg)`;
+    yelkovan2[0].style.transform=`rotate(${(minute/60)*360}deg)`;
+
+    saniye[0].style.transform=`rotate(${(second/60)*360}deg)`;
+    saniye2[0].style.transform=`rotate(${(second/60)*360}deg)`;
+}
+
+setInterval(zaman,1000);
